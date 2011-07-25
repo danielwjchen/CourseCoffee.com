2b05005ad69fa417fbd42265b774ec070e69776e
Daniel Chen <s1300045@gmail.com>
2011-07-25 08:17:21 -0400

    Created a template configuration file. Attempted to improve PHP configuration

---
+++ Web/default.config.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @file
+ * Template configuration file
+ *
+ * This file should be edited with installation specific values
+ */
+
+$config = new Config(array(
+	'db' => array(
+		/**
+		 * Define a database connection for system database
+		 */
+		'sys' => array(
+			'name' => 'example_sys',
+			'driver' => 'mysql',
+			'host' => 'localhost',
+			'user' => 'username',
+			'password' => 'password',
+			'port' => '3306',
+		),
+		/**
+		 * Define a database connection for core database
+		 */
+		'core' => array(
+			'name' => 'exmple_core',
+			'driver' => 'mysql',
+			'host' => 'localhost',
+			'user' => 'username',
+			'password' => 'password',
+			'port' => '3306',
+		),
+	),
+));
--- Web/includes/bootstrap.php
+++ Web/includes/bootstrap.php
@@ -21,3 +21,26 @@ require_once INCLUDES_PATH . '/Router.php';
 require_once INCLUDES_PATH . '/Logger.php';
 require_once INCLUDES_PATH . '/Crypto.php';
 require_once INCLUDES_PATH . '/Cookie.php';
+
+/**
+ * PHP settings:
+ *
+ * To see what PHP settings are possible, including whether they can
+ * be set at runtime (ie., when ini_set() occurs), read the PHP
+ * documentation at http://www.php.net/manual/en/ini.php#ini.list
+ * and take a look at the .htaccess file to see which non-runtime
+ * settings are used there. Settings defined here should not be
+ * duplicated there so as to avoid conflict issues.
+ini_set('arg_separator.output',     '&amp;');
+ini_set('magic_quotes_runtime',     0);
+ini_set('magic_quotes_sybase',      0);
+ini_set('session.cache_expire',     200000);
+ini_set('session.cache_limiter',    'none');
+ini_set('session.cookie_lifetime',  2000000);
+ini_set('session.gc_maxlifetime',   200000);
+ini_set('session.save_handler',     'user');
+ini_set('session.use_cookies',      1);
+ini_set('session.use_only_cookies', 1);
+ini_set('session.use_trans_sid',    0);
+ini_set('url_rewriter.tags',        '');
+ */
