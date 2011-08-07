d92ffd1cbdbd5e1ce79dcdac539c3077a16f4294
Daniel Chen <s1300045@gmail.com>
2011-08-07 17:14:41 -0400

    Applied layout to doc editor

--- Web/view/DocumentEditorPageView.php
+++ Web/view/DocumentEditorPageView.php
@@ -12,6 +12,7 @@ class DocumentEditorPageView extends PageView implements PageViewInterface {
 		parent::__construct($data);
 		$this->setPageTitle('editor');
 		$this->addJS('controller/editor.js');
+		$this->addJS('lib/date.js');
 		$this->addCSS('editor.css');
 	}
 
@@ -21,32 +22,53 @@ class DocumentEditorPageView extends PageView implements PageViewInterface {
 	public function getContent() {
 		extract($this->data);
 		return <<<HTML
-	<form id="processor-dorm" name="document-processor">
-		<input type="hidden" name="document" value="{$document}" />
-		<input type="hidden" name="token" value="{$processor_token}" />
-	</form>
-<div id="parsed_data">parsed_data</div>
-
-<table width="500"  cellpadding="0" cellspacing="0" id="table_syl">
-  <tr>
-    <td height="30" id="table_title" width="500">Original Text</td>
-  </tr>
-  <tr>
-    <td width="500"><div id="orig_syl"></div></td>
-  </tr>
-</table>
-
-<div id="tool_box">
-  <table width="500" height="30" cellpadding="0" cellspacing="0">
-    <tr>
-      <td width="49" class="tool_box_btn" id="undo">undo</td>
-      <td width="49" class="tool_box_btn" id="redo">redo</td>
-      <td width="81" class="tool_box_btn" >new schedule</td>
-      <td width="319"></td>
-      
-    </tr>
-  </table>
-</div>
+<div class="editor container">
+	<div class="container-inner">
+		<div class="header">
+			<div class="header-inner">
+				<ul id="navigation-menu">
+					<li class="home active">
+						<a class="home button" href="/home">Home</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="body">
+			<div class="body-inner">
+				<div class="content">
+					<form id="processor-dorm" name="document-processor">
+						<input type="hidden" name="document" value="{$document}" />
+						<input type="hidden" name="token" value="{$processor_token}" />
+					</form>
+					<div id="parsed_data">parsed_data</div>
+						<table width="460"  cellpadding="0" cellspacing="0" id="table_syl">
+							<tr>
+								<td height="30" id="table_title" width="500">Original Text</td>
+							</tr>
+							<tr>
+								<td width="460"><div id="orig_syl"></div></td>
+							</tr>
+						</table>
+						<div id="tool_box">
+							<table width="460" height="30" cellpadding="0" cellspacing="0">
+								<tr>
+									<td width="49" class="tool_box_btn" id="undo">undo</td>
+									<td width="49" class="tool_box_btn" id="redo">redo</td>
+									<td width="81" class="tool_box_btn" >new schedule</td>
+									<td width="279"></td>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="footer-inner">
+			{$footer['block']}
+		</div>
+	</div>
 HTML;
 	}
 }
