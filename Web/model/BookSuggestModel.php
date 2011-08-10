fba49ed385f770016aa75dbffcf474f57cab8c10
Daniel Chen <s1300045@gmail.com>
2011-08-10 13:30:56 -0400

    Added book title and cover image url. Turend off debug outputs

--- Web/model/BookSuggestModel.php
+++ Web/model/BookSuggestModel.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @file
+ * Suggest list of books for a class
+ */
 
 require_once LIB_PATH . '/booksearcher/AmazonAPI.php';
 require_once LIB_PATH . '/booksearcher/BarnesNobleAPI.php';
@@ -6,10 +10,6 @@ require_once LIB_PATH . '/booksearcher/BookRenterAPI.php';
 require_once LIB_PATH . '/booksearcher/eCampusAPI.php';
 require_once LIB_PATH . '/booksearcher/ValoreBooksAPI.php';
 
-/**
- * @file
- * Suggest list of books for a class
- */
 class BookSuggestModel extends Model {
 
 	/**
@@ -18,46 +18,75 @@ class BookSuggestModel extends Model {
 	private $book_list;
 
 	private $list;
+	private $amazonSearch;
+
+	/**
+	 * Extend Model::__construct()
+	 */
+	function __construct() {
+		parent::__construct();
+		$this->amazonSearch = new AmazonAPI();
+	}
+
 
 	/**
 	 * Get book list
 	 *
 	 * @param $section_id
+	 *
+	 * @return array
+	 *  the returned variable must be of array type!
 	 */
 	public function getBookList($section_id) {
 		$this->book_list = new BookListDAO($this->db);
-		$has_no_reading = $this->book_list->read(array('section_id' => $section_id));
-		error_log('asdfsadf' . print_r($this->book_list->list, true));
+		$has_reading = $this->book_list->read(array('section_id' => $section_id));
+		
+		// debug
+		// error_log('asdfsadf' . print_r($this->book_list->list, true));
 
-		if ($has_no_reading) {
+		if (!$has_reading) {
 			//return $this->list;
-			return "no reading";
+			return array('message' => 'no reading');
 		}
 		// the system truncates the list if there is only one record... we need to 
 		// restore it back
+		$record = array();
 		if (isset($this->book_list->list['isbn'])) {
 			$record[0] = $this->book_list->list;
 		} else {
 			$record = $this->book_list->list;
 		}
 
-		//return $record;
+		// debug
+		// error_log('book suggest record - ' . print_r($record, true));
+
 		$this->list = array();	
 
 		//$this->list = array($record[0]['isbn'],$this->getSingleBookRankList($record[0]['isbn']));
 
 		for ($i = 0; $i < count($record); $i++) {
 			$isbn = $record[$i]['isbn'];
-			array_push($this->list,array($isbn, $this->getSingleBookRankList($isbn)));
+			$this->amazonSearch->searchBookIsbn($isbn);
+			$title = (string)$this->amazonSearch->getTitle();
+			$image = (string)$this->amazonSearch->getSmallImageLink(); 
+
+			$this->list[] = array(
+				$title => array(
+					'image'  => $image,
+					'offers' => $this->getSingleBookRankList($isbn),
+				),
+			);
 		}
 
+		// debug
+		// error_log('book suggest result - ' . print_r($this->list, true));
+
 		return $this->list;
-		//return "have result";
 	}
 
 
-	/*
-	 *save information into list->new   ->storeXX->price
+	/**
+	 * Save information into list->new   ->storeXX->price
 	 *                                        ->link
 	 *                                  ->storeYY->price
 	 *            	                          ->link
@@ -66,8 +95,6 @@ class BookSuggestModel extends Model {
 	 */
 	public function getSingleBookRankList($isbn){
 
-		$amazonSearch = new AmazonAPI();
-		$amazonSearch->searchBookIsbn($isbn);
 		$ecampusSearch = new eCampusAPI($isbn);
 		$bookrenterSearch = new BookRenterAPI($isbn);
 		$valorebookSearch = new ValoreBooksAPI($isbn);
@@ -75,20 +102,20 @@ class BookSuggestModel extends Model {
 		//start to querysubstr($ecampusSearch->getLowestNewPrice(),0,strlen($ecampusSearch->getLowestNewPrice())),
 		//new
 		$newprice = array(
-			'Amazon'	=> substr($amazonSearch->getLowestNewPrice(),1,strlen($amazonSearch->getLowestNewPrice())),
+			'Amazon'	=> substr($this->amazonSearch->getLowestNewPrice(),1,strlen($this->amazonSearch->getLowestNewPrice())),
 			'eCampus'	=> substr($ecampusSearch->getLowestNewPrice(),0,strlen($ecampusSearch->getLowestNewPrice())),
 			'BookRenter'	=> substr($bookrenterSearch->getLowestNewPrice(),1,strlen($bookrenterSearch->getLowestNewPrice())),
 			'ValoreBooks'   => substr($valorebookSearch->getLowestNewPrice(),1,strlen($valorebookSearch->getLowestNewPrice())),
-			'AmazonMarket'  => substr($amazonSearch->getMarketPlaceLowestNewPrice(),1,strlen($amazonSearch->getMarketPlaceLowestNewPrice())),
+			'AmazonMarket'  => substr($this->amazonSearch->getMarketPlaceLowestNewPrice(),1,strlen($this->amazonSearch->getMarketPlaceLowestNewPrice())),
 			'eCampusMArket' => substr($ecampusSearch->getLowestMarketPlacePrice(),0,strlen($ecampusSearch->getLowestMarketPlacePrice()))
 		);
 
 		$newlink = array(
-			'Amazon'	=> $amazonSearch->getLowestNewLink(),
+			'Amazon'	=> $this->amazonSearch->getLowestNewLink(),
 			'eCampus'	=> $ecampusSearch->getLowestNewLink(),
 			'BookRenter'	=> $bookrenterSearch->getLowestNewLink(),
 			'ValoreBooks'   => $valorebookSearch->getLowestNewLink(),
-			'AmazonMarket'  => $amazonSearch->getLowestNewLink(),
+			'AmazonMarket'  => $this->amazonSearch->getLowestNewLink(),
 			'eCampusMArket' => $ecampusSearch->getLowestMarketPlaceLink()
 		);
 
@@ -96,12 +123,12 @@ class BookSuggestModel extends Model {
 		$usedprice = array(
 			'eCampus'	=> substr($ecampusSearch->getLowestUsedPrice(),0,strlen($ecampusSearch->getLowestUsedPrice())),
 			'BookRenter'	=> substr($bookrenterSearch->getLowestUsedPrice(),1,strlen($bookrenterSearch->getLowestUsedPrice)),
-			'AmazonMarket'  => substr($amazonSearch->getMarketPlaceLowestUsedPrice(),1,strlen($amazonSearch->getMarketPlaceLowestUsedPrice()))		
+			'AmazonMarket'  => substr($this->amazonSearch->getMarketPlaceLowestUsedPrice(),1,strlen($this->amazonSearch->getMarketPlaceLowestUsedPrice()))		
 		);
 		$usedlink = array(
 			'eCampus'	=> $ecampusSearch->getLowestUsedLink(),
 			'BookRenter'	=> $bookrenterSearch->getLowestUsedLink(),
-			'AmazonMarket'  => $amazonSearch->getLowestNewLink()
+			'AmazonMarket'  => $this->amazonSearch->getLowestNewLink()
 		);
 
 		//rental
