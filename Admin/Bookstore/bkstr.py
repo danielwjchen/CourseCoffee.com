6850160182448e377943bc5fc5ac5a4c8cae05d1
Daniel Chen <s1300045@gmail.com>
2011-10-25 08:58:49 -0400

    Finished re-running EMich. Organized bookstore related scripts.

--- Admin/Bookstore/bkstr.py
+++ Admin/Bookstore/bkstr.py
@@ -7,7 +7,6 @@ bkstr.com has the most strict security measure yet. It uses cookies to control
 each session and limits the number and frequency of queries for each session. 
 
 """
-import pycurl
 import re
 import StringIO
 import urllib
@@ -17,7 +16,7 @@ import time
 import Bookstore
 
 
-class BKSTR:
+class BKSTR(Bookstore.BaseClass):
     """Query data from bkstr.com
     
     to-do - Implement a generic parent class
@@ -25,10 +24,8 @@ class BKSTR:
     """
 
     def __init__(self):
-        """default constructor"""
-        self.params    = {}
-        """firefox 7"""
-        self.userAgent = "Mozilla/5.0 (Windows NT 6.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1"
+        """Extend BaseClass::__init__"""
+        super(BKSTR, self).__init__()
         self.referer   = 'http://www.bkstr.com/CategoryDisplay/10001-9604-15453-1?demoKey=d'
         """URL to the store page for the requested college"""
         self.storeURL = 'http://www.bkstr.com/webapp/wcs/stores/servlet/StoreCatalogDisplay'
@@ -36,30 +33,6 @@ class BKSTR:
         self.curriculumURL = 'http://www.bkstr.com/webapp/wcs/stores/servlet/LocateCourseMaterialsServlet'
         """URL to page that conatins item (book title) information"""
         self.itemURL = 'http://www.bkstr.com/webapp/wcs/stores/servlet/CourseMaterialsResultsView'
-        self.cookieFile  = 'cookies.txt'
-        """Create log file for error handling"""
-        self.errorLogFile = 'log'
-
-    def getParam(self, name):
-        """Get the value of parameter passed as part of the query"""
-        return self.params[name]
-
-    def setParam(self, name, value):
-        """Set the value of parameter passed as part of the query"""
-        self.params[name] = value
-
-    def handleError(self, message):
-        """Log error in file"""
-        timestring = time.strftime("%y/%m/%d %H:%M", time.gmtime())
-        log = timestring + " - " + message
-        print log
-        errorLog = open(self.errorLogFile, 'w+')
-        errorLog.write(log)
-        errorLog.close()
-
-    def encodeRequest(self):
-        """Encode params to url string"""
-        return urllib.urlencode(self.params)
 
     def beginSession(self):
         """begin session with bkstr.com
@@ -69,67 +42,24 @@ class BKSTR:
         
         """
         cookieFile = open(self.cookieFile, 'w+')
-        request = self.storeURL + "?storeId=" + self.getParam('storeId')
-        self.sendRequest(request)
+        request = self.storeURL + "?storeId=" + super(BKSTR, self).getParam('storeId')
+        super(BKSTR, self).sendRequest(request)
         return
-        
-
-    def sendRequest(self, url):
-        """Send request to site for data
-
-        param string url
-
-        return string result
-            It could be in XML, HTML, or JSON depending on the site
-        """
-        retried  = 0
-        retryMax = 5
-        success  = False
-        while retried < retryMax and success is False:
-            try:
-                curl = pycurl.Curl()
-                curl.setopt(pycurl.USERAGENT, self.userAgent)
-                curl.setopt(pycurl.URL, url)
-                curl.setopt(pycurl.FOLLOWLOCATION, 1)
-                curl.setopt(pycurl.MAXREDIRS, 500)
-                curl.setopt(pycurl.COOKIEFILE, self.cookieFile) 
-                curl.setopt(pycurl.COOKIEJAR, self.cookieFile) 
-                curl.setopt(pycurl.REFERER, self.referer)
-                stringIO = StringIO.StringIO()
-                curl.setopt(pycurl.WRITEFUNCTION, stringIO.write)
-                curl.perform()
-                curl.close()
-                result = stringIO.getvalue()
-                stringIO.close()
-                success = True
-                """pause execution so we don't get caught"""
-                time.sleep(5)
-            except:
-                retried = retried + 1
-                self.handleError('curl failed')
-                continue
-            break
-
-        if retried == retryMax and success is False:
-            self.handleError('curl could not be completed for request - ' + url)
-            return
-
-        return result
 
     def setInstitution(self):
         """Set institution ids"""
-        self.setParam('storeId', '15453')
-        self.setParam('programId', '1607')
-        self.setParam('termId', '100019123')
-        self.setParam('divisionName', ' ')
-        self.setParam('demoKey', 'd')
-        self.setParam('_', '')
+        super(BKSTR, self).setParam('storeId', '15453')
+        super(BKSTR, self).setParam('programId', '1607')
+        super(BKSTR, self).setParam('termId', '100019123')
+        super(BKSTR, self).setParam('divisionName', ' ')
+        super(BKSTR, self).setParam('demoKey', 'd')
+        super(BKSTR, self).setParam('_', '')
 
     def getSubjectList(self):
         """Get subject list"""
-        self.setParam('requestType', 'DEPARTMENTS')
-        request = self.curriculumURL + '?' + self.encodeRequest()
-        text= self.sendRequest(request)
+        super(BKSTR, self).setParam('requestType', 'DEPARTMENTS')
+        request = self.curriculumURL + '?' + super(BKSTR, self).encodeRequest()
+        text = super(BKSTR, self).sendRequest(request)
         text = text.replace("<script>parent.doneLoaded('", '')
         text = text.replace("')</script>", '')
         jsonResult = json.loads(text)
@@ -141,10 +71,10 @@ class BKSTR:
 
     def getCourseList(self, subject):
         """Get course list for a given subject."""
-        self.setParam('requestType', 'COURSES')
-        self.setParam('departmentName', subject)
-        request = self.curriculumURL + '?' + self.encodeRequest()
-        text= self.sendRequest(request)
+        super(BKSTR, self).setParam('requestType', 'COURSES')
+        super(BKSTR, self).setParam('departmentName', subject)
+        request = self.curriculumURL + '?' + super(BKSTR, self).encodeRequest()
+        text = super(BKSTR, self).sendRequest(request)
         text = text.replace("<script>parent.doneLoaded('", '')
         text = text.replace("')</script>", '')
         jsonResult = json.loads(text)
@@ -156,11 +86,11 @@ class BKSTR:
 
     def getSectionList(self, subject, course):
         """Get section list for a given course."""
-        self.setParam('requestType', 'SECTIONS')
-        self.setParam('departmentName', subject)
-        self.setParam('courseName', course)
-        request = self.curriculumURL + '?' + self.encodeRequest()
-        text= self.sendRequest(request)
+        super(BKSTR, self).setParam('requestType', 'SECTIONS')
+        super(BKSTR, self).setParam('departmentName', subject)
+        super(BKSTR, self).setParam('courseName', course)
+        request = self.curriculumURL + '?' + super(BKSTR, self).encodeRequest()
+        text = super(BKSTR, self).sendRequest(request)
         text = text.replace("<script>parent.doneLoaded('", '')
         text = text.replace("')</script>", '')
         jsonResult = json.loads(text)
@@ -187,18 +117,18 @@ class BKSTR:
         tried = 0
         retryMax = 5
         success = False
+        super(BKSTR, self).setParam('purpose', 'browse')
+        super(BKSTR, self).setParam('catalogId', '10001')
+        super(BKSTR, self).setParam('categoryId', '9604')
+        super(BKSTR, self).setParam('requestType', 'SECTIONS')
+        super(BKSTR, self).setParam('divisionDisplayName', ' ')
+        super(BKSTR, self).setParam('departmentDisplayName', subject)
+        super(BKSTR, self).setParam('courseDisplayName', course)
+        super(BKSTR, self).setParam('sectionDisplayName', section)
+        request =  self.itemURL + '?' + super(BKSTR, self).encodeRequest()
         while tried < retryMax and success is not True:
-            self.setParam('purpose', 'browse')
-            self.setParam('catalogId', '10001')
-            self.setParam('categoryId', '9604')
-            self.setParam('requestType', 'SECTIONS')
-            self.setParam('divisionDisplayName', ' ')
-            self.setParam('departmentDisplayName', subject)
-            self.setParam('courseDisplayName', course)
-            self.setParam('sectionDisplayName', section)
-            request =  self.itemURL + '?' + self.encodeRequest()
-            text = self.sendRequest(request)
-            if re.findall("cmCreateErrorTag", text):
+            text = super(BKSTR, self).sendRequest(request)
+            if not text or re.findall(r"cmCreateErrorTag", text):
                 self.beginSession()
                 tried += 1
             else:
@@ -206,7 +136,7 @@ class BKSTR:
 
 
         if tried == retryMax and success is not True:
-            self.handleError(subjct + " " + course + " " + section + " " + "FAILED")
+            super(BKSTR, self).handleError(subjct + " " + course + " " + section + " " + "FAILED")
                 
 
         """
@@ -231,12 +161,8 @@ class BKSTR:
         return result
 
 
-    def querySite(self):
-        """Query site for data"""
-        return
-
 bookStore  = BKSTR()
-listWriter = Bookstore.ListWriter('EMICH_bkstr')
+listWriter = Bookstore.XMLWriter('EMICH_bkstr')
 bookStore.setInstitution()
 bookStore.beginSession()
 subjectList = bookStore.getSubjectList()
